b1d50a55ac022eb61fd59a3df7b3d83cd55e0d09
grdhog <grdhog@users.noreply.github.com>
2023-02-06 13:55:28 +1300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mark-key-master
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue2-vite-starter-dr9bzj)
---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  mounted: function () {
+    console.log('mounted');
+    let aDate = new Date();
+    aDate.setFullYear(aDate.getFullYear() - 1);
+    this.period1 = aDate.getTime();
+    this.period2 = Date.now();    
+  },
+  data: {
+    csv: '',
+    period1: 0,
+    period2: 0,
+    closing: []
+  },
+  methods: {
+    convertCSV: function(){
+      const THE_DATE = 0;
+      const ADJUSTED_CLOSE = 5;
+      const lines = this.csv.split('\n');
+      console.log('num lines', lines.length);
+      const headers = lines.shift();
+      console.log('headers', headers);
+      const array = [];
+      for (const line of lines){
+        let parts = line.split(',');
+        array.push({'date': parts[THE_DATE], 'price': parts[ADJUSTED_CLOSE]});
+      }
+      this.closing = array;
+    },
+    getTicker: function () {
+      let ticker = this.$refs.ticker.value.trim();    
+      let params = {};
+      params.period1 = this.period1;
+      params.period2 = this.period2;
+      params.ticker = ticker;
+      const qs = '?' + new URLSearchParams(params).toString();
+      var url = 'https://hello-get-n5tpywpmka-uc.a.run.app' + qs;
+      console.log('url', url)
+      var oReq = new XMLHttpRequest()
+      var that = this
+      oReq.addEventListener('load', function () {
+        that.csv = this.responseText;
+        that.convertCSV();
+      })
+      oReq.addEventListener('error', function () {
+        console.error('API not working!')
+      })
+      oReq.open('GET', url)
+      oReq.send()
+    },
+  },
+})
+</script>
+
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<style></style>
